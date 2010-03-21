c447f0465e646f883955639182bee025f03251f1
Diego Iastrubni <diegoiast@gmail.com>
2010-03-21 20:15:45 +0000

    demo has support for loading files send to it's argv(1). yay!, it's actually usable now.

--- tools/qtsourceview/demos/demo5/main5.cpp
+++ tools/qtsourceview/demos/demo5/main5.cpp
@@ -3,55 +3,74 @@
 #include <QToolBar>
 #include <QTimer>
 #include <QMainWindow>
+#include <QFileDialog>
 
 #include "qsvtextedit.h"
 #include "qsvsyntaxhighlighterbase.h"
 #include "qsvtextoperationswidget.h"
 
+class MainWindow : QMainWindow
+{
+	Q_OBJECT
+public:
+	MainWindow( const QString &file )
+	{
+		QsvSyntaxHighlighterBase *s             = new QsvSyntaxHighlighterBase;
+		QsvTextEdit              *e             = new QsvTextEdit(this, s);
+		QsvTextOperationsWidget *textOpetations = new QsvTextOperationsWidget(e);
+	
+		QToolBar *b = addToolBar( "" );
+		b->addAction( tr("Open"), this, SLOT(loadFile()))
+		 ->setShortcut(QKeySequence("Ctrl+O"));
+		b->addAction( tr("Find"), textOpetations, SLOT(showSearch()))
+		 ->setShortcut(QKeySequence("Ctrl+F"));
+		b->addAction( tr("Replace"), textOpetations, SLOT(showReplace()))
+		 ->setShortcut(QKeySequence("Ctrl+R"));
+		b->addAction( tr("Fing next"), textOpetations, SLOT(searchNext()))
+		 ->setShortcut(QKeySequence("F3"));
+		b->setMovable(false);
+		b->addAction( tr("Fing prev"), textOpetations, SLOT(searchPrev()))
+				->setShortcut(QKeySequence("Shift+F3"));
+		b->setMovable(false);
+		
+		qDebug("Loading file %s", qPrintable(file) );
+		e->loadFile(file);
+		
+		setCentralWidget(e);
+		showMaximized();
+
+#if 0
+		// tests for defaults
+		e->setShowLineNumbers(true);
+		e->setShowMargins(true);
+		e->setTabSize(8);
+		e->setTabIndents(true);
+		e->setInsertSpacesInsteadOfTabs(true);
+		e->setShowWhiteSpace(true);
+#endif
+	}
+	
+public slots:
+	void loadFile()
+	{
+		QString s = QFileDialog::getOpenFileName(this,tr("Load file"));
+		if (s.isEmpty())
+			return;
+		QsvTextEdit *e = findChild<QsvTextEdit*>();
+		if (!e)
+			return;
+		e->loadFile(s);
+	}
+
+private:
+};
+
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	QMainWindow w;
-	
-	QsvSyntaxHighlighterBase *s             = new QsvSyntaxHighlighterBase;
-	QsvTextEdit              *e             = new QsvTextEdit(&w, s);
-	QsvTextOperationsWidget *textOpetations = new QsvTextOperationsWidget(e);
-
-	QToolBar *b = w.addToolBar( "" );
-	b->addAction( w.tr("Find"), textOpetations, SLOT(showSearch()))
-	 ->setShortcut(QKeySequence("Ctrl+F"));
-	b->addAction( w.tr("Replace"), textOpetations, SLOT(showReplace()))
-	 ->setShortcut(QKeySequence("Ctrl+R"));
-	b->addAction( w.tr("Fing next"), textOpetations, SLOT(searchNext()))
-	 ->setShortcut(QKeySequence("F3"));
-	b->setMovable(false);
-	b->addAction( w.tr("Fing prev"), textOpetations, SLOT(searchPrev()))
-			->setShortcut(QKeySequence("Shift+F3"));
-	b->setMovable(false);
-
-#if 0
-	e->setPlainText(
-"#include <stdio.h>\n\n"
-"int main()       \n"
-"{       \n"
-"	{ printf(\"Hello world!\\n\"); }           \n"
-"		}\n" );
-	e->removeModifications();
-#else
-	e->loadFile("qsvtextedit.cpp");
-//	e->newDocument();
-#endif
-	
-	// tests for defaults
-	e->setShowLineNumbers(true);
-	e->setShowMargins(true);
-	e->setTabSize(8);
-	e->setTabIndents(true);
-	e->setInsertSpacesInsteadOfTabs(true);
-	e->setShowWhiteSpace(true);
-		
-	w.setCentralWidget(e);
-	w.showMaximized();
+	MainWindow w( a.arguments().count()>=2 ? a.arguments().at(1) : QString() );
 	a.connect(&a, SIGNAL(lastWindowClosed()), &a, SLOT(quit()));
 	return a.exec();
 }
+
+#include "main5.moc"
