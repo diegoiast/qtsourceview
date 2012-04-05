975de8172e671dfa69475d8c83ba40082a24c966
Diego Iastrubni <diegoiast@gmail.com>
2012-04-05 21:51:27 +0000

    Qate demo is now built by default
    The Qate library is built by default now and it builds 2 demos (first to be obsoleted soon). All using cmake.

--- tools/qtsourceview/demos/demo-qate2/demo-qate2.h
+++ tools/qtsourceview/demos/demo-qate2/demo-qate2.h
@@ -1,3 +1,15 @@
+/**
+ * @brief demo-qate2 is a small demostration of how to Use
+ *
+ * This example shows how to integrate the qate highlighter into your application.
+ * The demo is made as simple as possible (code is embedded inside the heade
+ * for simplicity.
+ *
+ * The magic code is found in the constructor of the class,
+ */
+
+// $Id$
+
 #include <QObject>
 #include <QMainWindow>
 #include <QPlainTextEdit>
@@ -5,25 +17,15 @@
 #include <QIODevice>
 #include <QDir>
 #include <QToolBar>
+#include <QApplication>
+#include <QMessageBox>
+#include <QFileDialog>
 
 #include "context.h"
 #include "highlighter.h"
 #include "highlightdefinition.h"
 #include "qate/highlightdefinitionmanager.h"
 #include "qate/defaultcolors.h"
-
-#ifdef __WIN32
-#   include <windows.h>
-#   define LANGUAGE  QDir::homePath() + "\\AppData\\Roaming\\Nokia\\qtcreator\\generic-highlighter\\cpp.xml"
-#else
-#   define LANGUAGE  "/usr/share/kde4/apps/katepart/syntax/cpp.xml"
-#endif
-#define TEST_FILE "demos/demo6/demo6.cpp"
-
-/* some examples for different formats inside comments */
-// fixme todo  ###
-/// doxygen comment
-/// \brief bla bla bala
 
 void load_text(QString fe, QPlainTextEdit *te );
 QSharedPointer<TextEditor::Internal::HighlightDefinition> get_highlighter_definition(QString definitionFileName);
@@ -37,14 +39,19 @@ protected:
 	Qate::MimeDatabase *mimes;
 	Qate::HighlightDefinitionManager *hl_manager;
 	TextEditor::Internal::Highlighter *highlighter;
+	TextEditor::Internal::Context dummyContext;
+	QSharedPointer<TextEditor::Internal::Context> pdummyContext;
 	QSharedPointer<TextEditor::Internal::HighlightDefinition>  highlight_definition;
+
+	// this is a hack, otherwise the app crashes
+	bool highlightReady;
 public:
-	Demo2MainWindow(QWidget *parent=NULL) : QMainWindow(parent)
+	Demo2MainWindow(QWidget *parent=NULL) : QMainWindow(parent), pdummyContext(&dummyContext)
 	{
 		createMainGUI();
-		
+		highlightReady = false;
 		mimes       = new Qate::MimeDatabase();
-		highlighter = new TextEditor::Internal::Highlighter(textEditor->document());
+		highlighter = new TextEditor::Internal::Highlighter;
 		Qate::DefaultColors::ApplyToHighlighter(highlighter);
 		hl_manager = Qate::HighlightDefinitionManager::instance();
 		connect(hl_manager,SIGNAL(mimeTypesRegistered()), this, SLOT(onDefinitionsMetaDataReady()));
@@ -62,36 +69,150 @@ public:
 		a = b->addAction(tr("&Open"), this, SLOT(onOpen()));
 		a = b->addAction(tr("&Save"), this, SLOT(onSave()));
 		b->addSeparator();
-		a = b->addAction(tr("&Quit"));
+		a = b->addAction(tr("&Quit"), this, SLOT(onQuit()));
 		
 		textEditor = new QPlainTextEdit;
-		textEditor->setFont(QFont("Courier new",10));
+		textEditor->setFont(QFont("Courier",10));
 		textEditor->setFrameStyle(QFrame::NoFrame);
 		setCentralWidget(textEditor);
+//		loadTextFile("demo-qate2.h");
 	}
 	
 public slots:
 	void onDefinitionsMetaDataReady()
 	{
-		const char* FILE = "PHP (HTML)";
+		/*
+		const char* FILE = "C++";
 		highlight_definition = hl_manager->definition(hl_manager->definitionIdByName(FILE));
 		if (highlight_definition.isNull()) {
 			qDebug("No definition found for %s", FILE);
 			return;
 		}
 		highlighter->setDefaultContext(highlight_definition->initialContext());
+		highlighter->setDocument(textEditor->document());
+		*/
+		highlightReady = false;
+		highlightReady = true;
+		loadTextFile("demo-qate2.h");
 	}
 	
 	void onNew()
 	{
 		qDebug("New");
 	}
+
 	void onOpen()
 	{
-		qDebug("Open");
+		QString fileName = QFileDialog::getOpenFileName(this,
+			tr("Open file"), "", tr(
+			"Source files (*.h * hpp *.c *.C *.cpp *.cxx *.pas);;"
+			"Scripts (*.pl *.py *.sh);;"
+			"All files (*)"
+		));
+
+		if (fileName.isEmpty())
+			return;
+		loadTextFile(fileName);
 	}
-	void onSave()
+
+	bool onSave()
 	{
-		qDebug("Save");
+		return false;
 	}
+
+	void onQuit()
+	{
+		if (textEditor->document()->isModified()) {
+			QMessageBox::StandardButton ret = QMessageBox::warning( this, tr("Qate - demo2"),
+				tr("The document has been modified.\n"
+				"Do you want to save your changes before closing?"),
+				QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel,
+				QMessageBox::Save
+			);
+
+			switch (ret) {
+				case QMessageBox::Save:
+					if (!onSave()) return;
+					break;
+				case QMessageBox::Discard:
+					break;
+				case QMessageBox::Cancel:
+					return;
+			}
+
+		}
+		QApplication::quit();
+	}
+
+	void loadTextFile(QString fileName)
+	{
+		QFile f(fileName);
+
+		if (!f.open(QIODevice::ReadOnly | QIODevice::Text))
+			return;
+		QString s = f.readAll();
+		textEditor->clear();
+		textEditor->setLineWrapMode(QPlainTextEdit::NoWrap);
+
+		if (highlightReady) {
+			Qate::MimeType m = mimes->findByFile(fileName);
+
+			if (m.isNull())
+				m = getMimeByExt(fileName);
+
+			QString definitionId = hl_manager->definitionIdByMimeType(m.type());
+			if (definitionId.isEmpty())
+				definitionId = findDefinitionId(m,true);
+			if (!definitionId.isEmpty()) {
+				qDebug("Using %s", qPrintable(definitionId));
+				highlight_definition = hl_manager->definition(hl_manager->definitionIdByMimeType(m.type()));
+				if (!highlight_definition.isNull()) {
+					highlighter->setDefaultContext(highlight_definition->initialContext());
+				} else {
+					delete highlighter;
+					highlighter = new TextEditor::Internal::Highlighter;
+					Qate::DefaultColors::ApplyToHighlighter(highlighter);
+					qDebug("Error loading %s", qPrintable(definitionId));
+				}
+			} else {
+				delete highlighter;
+				highlighter = new TextEditor::Internal::Highlighter;
+				Qate::DefaultColors::ApplyToHighlighter(highlighter);
+				qDebug("No definition found for %s", qPrintable(fileName));
+			}
+			highlighter->setDocument(textEditor->document());
+		}
+
+		textEditor->setPlainText(s);
+	}
+
+	QString findDefinitionId(const Qate::MimeType &mimeType, bool considerParents) const
+	{
+	    QString definitionId = hl_manager->definitionIdByAnyMimeType(mimeType.aliases());
+	    if (definitionId.isEmpty() && considerParents) {
+		definitionId = hl_manager->definitionIdByAnyMimeType(mimeType.subClassesOf());
+		if (definitionId.isEmpty()) {
+		    foreach (const QString &parent, mimeType.subClassesOf()) {
+			const Qate::MimeType &parentMimeType =  mimes->findByType(parent);
+			definitionId = findDefinitionId(parentMimeType, considerParents);
+		    }
+		}
+	    }
+	    return definitionId;
+	}
+
+	Qate::MimeType getMimeByExt(const QString &fileName)
+	{
+		QFileInfo fi(fileName);
+		QString extension = QString("*.%1").arg(fi.suffix());
+		foreach(Qate::MimeType mime, mimes->mimeTypes() ) {
+			foreach(Qate::MimeGlobPattern pattern, mime.globPatterns() ) {
+				if (extension == pattern.regExp().pattern())
+					return mime;
+			}
+		}
+		return Qate::MimeType();
+	}
+
+
 };
