74c5fe808a07bd4a61cdefdbf03240dca83f96db
Diego Iastrubni <diegoiast@gmail.com>
2018-09-08 21:48:44 +0300

    Lets add a script to download all defintions from upstream

---
+++ data/qate/fetch.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+ROOT=https://kate-editor.org/syntax/data/
+files=`wget $ROOT -q -O - | grep xml | awk '{print $5'} | cut -d\" -f 2`
+
+for f in $files; do
+    printf  "Downloading $f"
+    wget $ROOT/$f -q
+    printf ".\n"
+done
+
