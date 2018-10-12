0f2f9df99bbb855a7e2bb1231f2132873a2ac834
Diego Iastrubni <diegoiast@gmail.com>
2018-10-12 22:41:39 +0300

    Update fetch script and also download themes

--- data/qate/fetch.sh
+++ data/qate/fetch.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
+#set -x
+set -e
+
+# download syntax files
 ROOT=https://kate-editor.org/syntax/data/
 files=`wget $ROOT -q -O - | grep xml | awk '{print $5'} | cut -d\" -f 2`
 
@@ -9,3 +13,13 @@ for f in $files; do
     printf ".\n"
 done
 
+# download themes
+ROOT=https://kate-editor.org/syntax/data/themes/
+files=`wget $ROOT -q -O - | grep theme | awk '{print $6'} | cut -d\" -f 2`
+
+for f in $files; do
+    printf  "Downloading $f"
+    wget $ROOT/$f -q --no-clobber
+    printf ".\n"
+done
+mv *.theme ../colors/
