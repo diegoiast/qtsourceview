d103afb7bf4ccd09c2c6868681e28c1bfd5f9da4
Diego Iastrubni <diegoiast@gmail.com>
2018-10-14 13:39:47 +0300

    Rename file names + cleanups
    - Clean up obsolete code
    - Renamo demo6 to demo7
    - Rename demo-qate to demo6

--- demos/demo6/demo6.pro
+++ demos/demo6/demo6.pro
@@ -3,7 +3,9 @@ QT		=	gui core xml widgets network concurrent
 CONFIG		+=	qt warn_on silent
 DESTDIR		=	../../
 INCLUDEPATH	=	../../src .
-LIBS		=	-L../../ -lqate -lqsvte
-TARGET          =       demo6
+LIBS		=	-L../../ -lqate
+TARGET          =       demo6-qate
 SOURCES		=	main6.cpp
+
+# TODO - this should be removed
 DEFINES         +=      CORE_EXPORT=Q_DECL_EXPORT
--- demos/demo6/main6.cpp
+++ demos/demo6/main6.cpp
@@ -1,317 +1,132 @@
+/**
+ * \brief Sixth demo - Kate definitions based syntax highlighter
+ *
+ * The Kate project has a large amount of syntax definitions. The code
+ * has been ripped from QtCreator, and now can be used as a simple
+ * Qt application.
+ */
+
 #include <QApplication>
-#include <QAction>
-#include <QToolBar>
-#include <QTimer>
 #include <QMainWindow>
-#include <QWindow>
-#include <QFileDialog>
-#include <QSyntaxHighlighter>
-#include <QToolButton>
-#include <QPushButton>
-#include <QDebug>
-#include <QCommonStyle>
-#include <QTextDocument>
-#include <QTextBlock>
+#include <QPlainTextEdit>
+#include <QFile>
+#include <QIODevice>
+#include <QDir>
+
+#include <unistd.h>
 
 #include "qate/context.h"
 #include "qate/highlighter.h"
 #include "qate/highlightdefinition.h"
 #include "qate/highlightdefinitionmanager.h"
 #include "qate/defaultcolors.h"
-#include "qate/qateblockdata.h"
-#include "qate/mimedatabase.h"
 
-//#include "qate/qateblockdata.h"
-#include "qsvte/qsvtextedit.h"
-#include "qsvte/qsvtextoperationswidget.h"
-#include "qsvte/qsvsyntaxhighlighterbase.h"
+#ifdef __WIN32
+#   include <windows.h>
+#else
+#endif
 
+#define DATA_DIR "data/qate/"
+#define LANGUAGE  DATA_DIR "c.xml"
+#define TEST_FILE "demos/demo6/main6.cpp"
+#define LANG_NAME "C"
 
-class MyHighlighter: public TextEditor::Internal::Highlighter, public QsvSyntaxHighlighterBase
+/* some examples for different formats inside comments */
+// fixme todo  ###
+/// doxygen comment
+/// \brief bla bla bala
+
+void load_text(QString fe, QPlainTextEdit *te );
+QSharedPointer<TextEditor::Internal::HighlightDefinition> get_highlighter_definition(QString definitionFileName);
+
+int main( int argc, char* argv[] )
 {
-public:
-	MyHighlighter(QTextDocument *parent): TextEditor::Internal::Highlighter(parent)
-	{
-		setMatchBracketList("{}()[]''\"\"");
-	}
-
-	virtual void highlightBlock(const QString &text) override;
-
-	virtual void toggleBookmark(QTextBlock &block) override;
-
-	virtual void removeModification(QTextBlock &block) override;
-
-	virtual void setBlockModified(QTextBlock &block, bool on) override;
-
-	virtual bool isBlockModified(QTextBlock &block) override;
-
-	virtual bool isBlockBookmarked(QTextBlock &block) override;
-
-	virtual Qate::BlockData::LineFlags getBlockFlags(QTextBlock &block) override;
-
-	virtual void clearMatchData(QTextBlock &block) override;
-
-	virtual void addMatchData(QTextBlock &block, Qate::MatchData m) override;
-
-	virtual QList<Qate::MatchData> getMatches(QTextBlock &block) override;
-
-	virtual QTextBlock getCurrentBlockProxy() override;
-
-	Qate::BlockData *getBlockData(QTextBlock &block);
-};
-
-void MyHighlighter::highlightBlock(const QString &text) {
-	QsvSyntaxHighlighterBase::highlightBlock(text);
-	TextEditor::Internal::Highlighter::highlightBlock(text);
-}
-
-void MyHighlighter::toggleBookmark(QTextBlock &block) {
-	Qate::BlockData *data = getBlockData(block);
-	if (data == nullptr)
-		return;
-	data->toggleBookmark();
-}
-
-void MyHighlighter::removeModification(QTextBlock &block) {
-	Qate::BlockData *data = getBlockData(block);
-	if (data == nullptr)
-		return;
-	data->m_isModified = false;
-}
-
-void MyHighlighter::setBlockModified(QTextBlock &block, bool on) {
-	Qate::BlockData *data = getBlockData(block);
-	if (data == nullptr)
-		return;
-	data->m_isModified =  on;
-}
-
-bool MyHighlighter::isBlockModified(QTextBlock &block) {
-	Qate::BlockData *data = getBlockData(block);
-	if (data == nullptr)
-		return false;
-	return data->m_isModified;
-}
-
-bool MyHighlighter::isBlockBookmarked(QTextBlock &block) {
-	Qate::BlockData *data = getBlockData(block);
-	if (data == nullptr)
-		return 0;
-	return data->isBookmark();
-}
-
-Qate::BlockData::LineFlags MyHighlighter::getBlockFlags(QTextBlock &block) {
-	Qate::BlockData *data = getBlockData(block);
-	if (data == nullptr)
-		return Qate::BlockData::LineFlag::Empty;
-	return data->m_flags;
-}
-
-void MyHighlighter::clearMatchData(QTextBlock &block) {
-	Qate::BlockData *data = getBlockData(block);
-	if (data == nullptr)
-		return;
-}
-
-void MyHighlighter::addMatchData(QTextBlock &block, Qate::MatchData m) {
-	Qate::BlockData *data = getBlockData(block);
-	if (data == nullptr)
-		return;
-	data->matches << m;
-}
-
-QList<Qate::MatchData> MyHighlighter::getMatches(QTextBlock &block) {
-	Qate::BlockData *data = getBlockData(block);
-	if (data == nullptr)
-		return QList<Qate::MatchData>();
-	return data->matches;
-}
-
-QTextBlock MyHighlighter::getCurrentBlockProxy() {
-	return currentBlock();
-}
-
-Qate::BlockData *MyHighlighter::getBlockData(QTextBlock &block) {
-	QTextBlockUserData *userData  = block.userData();
-	Highlighter::BlockData  *blockData = nullptr;
-
-	if (userData == nullptr){
-		blockData =  new Highlighter::BlockData;
-		block.setUserData(blockData);
-	} else {
-		blockData = dynamic_cast<Highlighter::BlockData*>(userData);
-	}
-	return blockData;
-}
-
-
-void ApplyTheme(const Qate::Theme &theme, QsvTextEdit *editor, TextEditor::Internal::Highlighter *highlight) {
-	theme.applyToHighlighter(highlight);
-	theme.applyToEditor(editor);
-
-	QColor c;
-        c = theme.getEditorColor(/*Qate::*/EditorCodeFolding);
-	if (c.isValid())
-		editor->setPanelColor(c);
-        c = theme.getEditorColor(/*Qate::*/EditorCurrentLine);
-	if (c.isValid())
-		editor->setCurrentLineBackground(c);
-        c = theme.getEditorColor(/*Qate::*/EditorModifiedLines);
-	if (c.isValid())
-		editor->setModifiedColor(c);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-//
-class MainWindow :public  QMainWindow
-{
-	Q_OBJECT
-	QsvTextEdit *editor;
-	QsvTextOperationsWidget  *textOpetations;
+	QApplication app( argc, argv );
+	QMainWindow    *main_window = new QMainWindow;
+	QPlainTextEdit *text_editor;
 
 	Qate::Theme theme;
 	Qate::MimeDatabase                                        *mimes;
 	Qate::HighlightDefinitionManager                          *hl_manager;
-	MyHighlighter                                             *highlight;
+	TextEditor::Internal::Highlighter                         *highlighter;
 	QSharedPointer<TextEditor::Internal::HighlightDefinition>  highlight_definition;
-
-public:
-	MainWindow( const QString &file )
-	{
-		editor     = new QsvTextEdit(this, nullptr);
-		highlight  = new MyHighlighter(editor->document());
-		editor->setHighlighter(highlight);
-		editor->setFrameStyle(QFrame::NoFrame);
-		editor->displayBannerMessage(tr("Click \"Open\" if you dare"), 3);
-		editor->setFont( QFont("Courier new",14) );
-		theme.setupDefaultColors();
-//		theme.load("data/colors/default.theme");
-//		theme.load("data/colors/solarized-light.theme");
-                theme.load("data/colors/solarized-dark.theme");
-//                theme.load("data/colors/breeze-dark.theme");
-		ApplyTheme(theme, editor, highlight);
-
-		mimes      = new Qate::MimeDatabase();
-		hl_manager = Qate::HighlightDefinitionManager::instance();
-		hl_manager->setMimeDatabase(mimes);
-		hl_manager->registerMimeTypes();
-
-//		loadFile("demos/demo6/main6.cpp");
-
-		connect(hl_manager,SIGNAL(mimeTypesRegistered()),this,SLOT(setCPPHighlight()));
-
-		textOpetations = new QsvTextOperationsWidget(editor);
-		textOpetations->initSearchWidget();
-		textOpetations->initReplaceWidget();
-		setCentralWidget(editor);
-		setup_GUI();
-		showMaximized();
-
-		if (!file.isEmpty())
-			loadFile(file);
-		else {
-			setWindowTitle("QtSourceView/qate demo6 - qedit");
-			loadFile("tests/highlight.pas");
-		}
-#if 0
-		e->setMarkCurrentLine(false);
-		// tests for defaults
-		e->setShowLineNumbers(true);
-		e->setShowMargins(true);
-		e->setTabSize(8);
-		e->setTabIndents(true);
-		e->setInsertSpacesInsteadOfTabs(true);
-		e->setShowWhiteSpace(true);
-#endif
-	}
 	
-public slots:
-	void loadFile( QString filename ="" )
-	{
-		const QString dir;
-		if (filename.isEmpty()) {
-			filename = QFileDialog::getOpenFileName(this,tr("Load file"),dir);
-			if (filename.isEmpty())
-				return;
-		}
-//		TODO
-//		if (e->isModified){
-//			e->save();
-//		}
-		
-		editor->clear();
-//		langDefinition = QsvLangDefFactory::getInstanse()->getHighlight(filename);
-//		highlight->setHighlight(langDefinition);
-		editor->loadFile(filename);
-		editor->removeModifications();
-		setWindowTitle(filename);
+	// create the main widget
+	text_editor = new QPlainTextEdit(main_window);
+	highlighter   = new TextEditor::Internal::Highlighter(text_editor->document());
 
-		Qate::MimeType aa = mimes->findByFile(filename);
-		QString a1 = aa.type();
-		QString a2 = hl_manager->definitionIdByMimeType(a1);
-		highlight_definition = hl_manager->definition(a2);
-		if (highlight_definition != nullptr)
-			highlight->setDefaultContext(highlight_definition->initialContext());
-//		else
-//			highlight->setDefaultContext(nulptr);
-                highlight->light();
+	theme.setupDefaultColors();
+	theme.applyToHighlighter(highlighter);
+	text_editor->setFont( QFont("Courier new",10) );
+	
+	// create the highlighters manager
+	hl_manager = Qate::HighlightDefinitionManager::instance();
+
+	// here is where the magic starts:
+	//
+	// The first option is to ask the manager for a specific file:
+	//
+	//	highlight_definition = hl_manager->definition(LANGUAGE);
+	//
+	// This will load the forward declarations and includes, and will work perfectly.
+	// The downside, is that you still need to know the exact path, and if your
+	// definitions are spread in several dirs - this will be a pain.
+	//
+	// The recommended way is to ask for a definition by name, or by mimetype
+	// by calling definitionIdByAnyMimeType(). This example uses the former.
+	//
+	// Notes: 
+	//  - if the syntax you loaded contains several includes or forward
+	//    and you do not see them, this means that 
+	//    Qate::HighlightDefinitionManager::registerMimeTypes() failed
+	//  - if your application needs a single definition, you can there is 
+	//    not need to setup the mime database, nor register the mime types
+	//    see the first example.
+	
+#if 0
+	highlight_definition = hl_manager->definition(LANGUAGE);
+#else
+	mimes = new Qate::MimeDatabase();
+	hl_manager->setMimeDatabase(mimes);
+	hl_manager->registerMimeTypes();
+
+        // ugly - but... let the highlight manager build the mime DB
+        // in  real life, you should the the highlight when the signal
+        // definitionsMetaDataReady() is emmited
+        // this code just waits "up to" a second for the HL to be available
+	int timeout = 1000;
+        while (timeout != 0) {
+        #ifdef __WIN32
+            SleepEx(1,true);
+        #else
+	    usleep(1000);
+        #endif
+	    highlight_definition = hl_manager->definition( hl_manager->definitionIdByName(LANG_NAME) );
+            if (!highlight_definition.isNull())
+                break;
+            timeout --;
         }
+#endif
+        if (!highlight_definition.isNull())
+		highlighter->setDefaultContext(highlight_definition->initialContext());
 
-	void setCPPHighlight()
-	{
-                highlight_definition = hl_manager->definition( hl_manager->definitionIdByName("Pascal") );
-//		highlight_definition = hl_manager->definition( hl_manager->definitionIdByName("C++") );
-		if (highlight_definition.isNull()) {
-			editor->displayBannerMessage("No C++ highlight definition is found. If you are using QtCreator's defintion, please download also C++");
-			return;
-		}
-		highlight->setDefaultContext(highlight_definition->initialContext());
-                highlight->rehighlight();
+	if (argc == 1) {
+		load_text(TEST_FILE, text_editor);
+	} else {
+		load_text(argv[1], text_editor);
 	}
-
-	void setup_GUI()
-	{
-		QPushButton *p;
-		editor->findChild<QWidget*>("banner")
-		      ->findChild<QToolButton*>("closeButton")
-		      ->setIcon( style()->standardIcon(QStyle::SP_DockWidgetCloseButton) );
-
-		textOpetations->m_search->findChild<QAbstractButton*>("closeButton")->setIcon( style()->standardIcon(QStyle::SP_DockWidgetCloseButton) );
-		p = textOpetations->m_search->findChild<QPushButton*>("previousButton");
-		p->setIcon( style()->standardIcon(QStyle::SP_ArrowUp) );
-		p->setText("");
-		p->setFlat(true);
-		p->setAutoRepeat(true);
-		p = textOpetations->m_search->findChild<QPushButton*>("nextButton");
-		p->setIcon( style()->standardIcon(QStyle::SP_ArrowDown) );
-		p->setText("");
-		p->setFlat(true);
-		p->setAutoRepeat(true);
-
-		textOpetations->m_replace->findChild<QAbstractButton*>("closeButton")->setIcon( style()->standardIcon(QStyle::SP_DockWidgetCloseButton) );
-		p = textOpetations->m_search->findChild<QPushButton*>("previousButton");
-
-		QToolBar *b = addToolBar( "" );
-		b->addAction( tr("&New"), editor, SLOT(newDocument()))              ->setShortcut(QKeySequence("Ctrl+N"));
-		b->addAction( tr("&Open"), this, SLOT(loadFile()))                  ->setShortcut(QKeySequence("Ctrl+O"));
-		b->addAction( tr("&Save"), editor, SLOT(saveFile()))                ->setShortcut(QKeySequence("Ctrl+S"));
-		b->addAction( tr("&Find"), textOpetations, SLOT(showSearch()))      ->setShortcut(QKeySequence("Ctrl+F"));
-		b->addAction( tr("&Replace"), textOpetations, SLOT(showReplace()))  ->setShortcut(QKeySequence("Ctrl+R"));
-		b->addAction( tr("Find &next"), textOpetations, SLOT(searchNext())) ->setShortcut(QKeySequence("F3"));
-		b->setMovable(false);
-		b->addAction( tr("Find &prev"), textOpetations, SLOT(searchPrev())) ->setShortcut(QKeySequence("Shift+F3"));
-		b->setMovable(false);
-	}
-
-private:
-};
-
-int main(int argc, char *argv[])
-{
-	QApplication a(argc, argv);
-	MainWindow w( a.arguments().count()>=2 ? a.arguments().at(1) : QString() );
-	a.connect(&a, SIGNAL(lastWindowClosed()), &a, SLOT(quit()));
-	return a.exec();
+	main_window->setWindowTitle("Kate syntax highter test");
+	main_window->setCentralWidget(text_editor);
+	main_window->show();
+	return app.exec();
 }
 
-#include "main6.moc"
+void load_text(QString fe, QPlainTextEdit *te )
+{
+	QFile f(fe);
+	if (!f.open(QIODevice::ReadOnly | QIODevice::Text))
+		return;
+	QString s = f.readAll();
+	te->setPlainText(s);
+	te->setLineWrapMode(QPlainTextEdit::NoWrap);
+}
