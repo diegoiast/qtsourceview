247f72907fb80911f0d7a89dfdbc752dc2e8727e
Diego Iastrubni <diegoiast@gmail.com>
2018-10-17 21:04:18 +0300

    Remove if 0 - code works

--- src/qate/defaultcolors.h
+++ src/qate/defaultcolors.h
@@ -47,52 +47,6 @@ class QPlainTextEdit;
 
 namespace Qate {
 
-#if 0
-#define FormatNormal        "Normal"
-#define FormatKeyword       "Keyword"
-#define FormatDataType      "DataType"
-#define FormatDecimal       "DecVal"
-#define FormatBaseN         "BaseN"
-#define FormatFloat         "Float"
-#define FormatChar          "Char"
-#define FormatString        "String"
-#define FormatComment       "Comment"
-#define FormatAlert         "Alert"
-#define FormatError         "Error"
-#define FormatFunction      "Function"
-#define FormatRegionMarker  "RegionMarker"
-#define FormatOthers        "Others"
-
-#define EditorBackgroundColor              "background-color"
-#define EditorCodeFolding                  "code-folding"
-#define EditorBracketMatching              "bracket-matching"
-#define EditorCurrentLine                  "current-line"
-#define EditorIconBorder                   "icon-border"
-#define EditorIndentationLine              "indentation-line"
-#define EditorLineNumbers                  "line-numbers"
-#define EditorCurrentLineNumber            "current-line-number"
-#define EditorMarkBookmark                 "mark-bookmark"
-#define EditorMarkBreakpointActive         "mark-breakpoint-active"
-#define EditorMarkBreakpointReached        "mark-breakpoint-reached"
-#define EditorMarkBreakpointDisabled       "mark-breakpoint-disabled"
-#define EditorMarkExecution                "mark-execution"
-#define EditorMarkWarning                  "mark-warning"
-#define EditorMarkError                    "mark-error"
-#define EditorModifiedLines                "modified-lines"
-#define EditorReplaceHighlight             "replace-highlight"
-#define EditorSavedLines                   "saved-lines"
-#define EditorSearchHighlight              "search-highlight"
-#define EditorSelection                    "selection"
-#define EditorSeparator                    "separator"
-#define EditorSpellChecking                "spell-checking"
-#define EditorTabMarker                    "tab-marker"
-#define EditorTemplateBackground           "template-background"
-#define EditorTemplatePlaceholder          "template-placeholder"
-#define EditorTemplateFocusedPlaceholder   "template-focused-placeholder"
-#define EditorTemplateReadOnlyPlaceholder  "template-read-only-placeholder"
-#define EditorWordWrapMarker               "word-wrap-marker"
-#else
-
 static constexpr const char* FormatNormal = "Normal";
 static constexpr const char* FormatKeyword = "Keyword";
 static constexpr const char* FormatDataType = "DataType";
@@ -136,7 +90,6 @@ static constexpr const char* EditorTemplatePlaceholder  = "template-placeholder"
 static constexpr const char* EditorTemplateFocusedPlaceholder  = "template-focused-placeholder";
 static constexpr const char* EditorTemplateReadOnlyPlaceholder  = "template-read-only-placeholder";
 static constexpr const char* EditorWordWrapMarker = "word-wrap-marker";
-#endif
 
 class Theme
 {
